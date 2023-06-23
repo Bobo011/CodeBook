d96cbdb4f6f1d4f012fc4728e9dc5248772d4108
George <alximik15@gmail.com>
2023-06-23 12:21:26 +0400

    Added some tailwind css files

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,12 +1,13 @@
-import { Routes,Route } from "react-router-dom"
-import { HomePage,ProductsList } from "../pages"
+import { Routes, Route } from "react-router-dom";
+import { HomePage, ProductsList } from "../pages";
+
 export const AllRoutes = () => {
   return (
     <>
-     <Routes>
+    <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsList />} />
-     </Routes>
+    </Routes>
     </>
   )
 }
