0a244dbea8bccdd8714107589038d68ec7142745
George <alximik15@gmail.com>
2023-06-23 14:46:51 +0400

    Setting up testimonials

--- src/pages/Home/components/Testimonials.jsx
+++ src/pages/Home/components/Testimonials.jsx
@@ -1,13 +1,18 @@
 export const Testimonials = () => {
   return (
     <section className='my-20'>
+        {/* Section Title */}
         <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">Student About CodeBook</h1>    
+          {/* Testimonials Grid */}
         <div className="grid mb-8 rounded-lg border border-gray-200 shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
+            {/* Testimonial 1 */}
             <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+                 {/* Testimonial Content */}
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
                     <p className="my-4 font-light">If you care for your time, I hands down would go with this."</p>
                 </blockquote>
+                {/* Testimonial Author */}
                 <figcaption className="flex justify-center items-center space-x-3">
                     <img className="w-9 h-9 rounded-full" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=120&q=50" alt="user" />
                     <div className="space-y-0.5 font-medium dark:text-white text-left">
@@ -16,6 +21,7 @@ export const Testimonials = () => {
                     </div>
                 </figcaption>    
             </figure>
+            {/* Testimonial 2 */}
             <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-tr-lg border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Solid foundation for any project</h3>
@@ -29,6 +35,7 @@ export const Testimonials = () => {
                     </div>
                 </figcaption>    
             </figure>
+            {/* Testimonial 3 */}
             <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Mindblowing workflow</h3>
@@ -42,6 +49,7 @@ export const Testimonials = () => {
                     </div>
                 </figcaption>    
             </figure>
+            {/* Testimonial 4 */}
             <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-b-lg border-gray-200 md:rounded-br-lg dark:bg-gray-800 dark:border-gray-700">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Efficient Collaborating</h3>
