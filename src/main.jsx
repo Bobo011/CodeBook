b23b9c6f3eff48d82f399549b5453243a93b4582
George <alximik15@gmail.com>
2023-06-24 20:23:24 +0400

    Added Scroll to Top

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { ScrollToTop } from "./components";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+    <ScrollToTop />
     <App />
     </Router>
     
