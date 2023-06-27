85280c23e08a765aaf3883df74c4c44b117cd4bf
George <alximik15@gmail.com>
2023-06-27 20:31:37 +0400

    Finished DropDown for User

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -2,45 +2,61 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.png";
 import { Search } from "../Sections/Search";
+import {DropdownLoggedOut} from "../index"
 
 export const Header = () => {
-  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
+  const [darkMode, setDarkMode] = useState(
+    JSON.parse(localStorage.getItem("darkMode")) || false
+  );
 
-const [search,setSearch] =useState(false)
+  const [search, setSearch] = useState(false);
+
+  const [dropDown,setDropdown]= useState(false)
+
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
-    
-    if(darkMode){
+
+    if (darkMode) {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
   }, [darkMode]);
-  
-
 
   return (
-    <header>      
+    <header>
       <nav className="bg-dark dark:bg-white">
-          <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
-              <Link to="/" className="flex items-center">
-                  <img src={Logo} className="mr-3 h-10" alt="CodeBook Logo" />
-                  <span className="self-center text-2xl text-gray-300 font-semibold whitespace-nowrap dark:text-white">CodeBook</span>
-              </Link>
-              <div className="flex items-center relative">
-                  <span onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-xl text-gray-300 dark:text-white mr-5 bi bi-gear-wide-connected"></span>
-                  <span onClick={()=>setSearch(!search)} className="cursor-pointer text-xl text-gray-300 dark:text-white mr-5 bi bi-search"></span>
-                  <Link to="/cart" className="text-gray-300 dark:text-white mr-5">
-                    <span className="text-2xl bi bi-cart-fill relative">
-                      <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">0</span>
-                    </span>                    
-                  </Link>
-                  <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-300 dark:text-white"></span>
-              </div>
+        <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
+          <Link to="/" className="flex items-center">
+            <img src={Logo} className="mr-3 h-10" alt="CodeBook Logo" />
+            <span className="self-center text-2xl text-gray-300 font-semibold whitespace-nowrap dark:text-white">
+              CodeBook
+            </span>
+          </Link>
+          <div className="flex items-center relative">
+            <span
+              onClick={() => setDarkMode(!darkMode)}
+              className="cursor-pointer text-xl text-gray-300 dark:text-white mr-5 bi bi-gear-wide-connected"
+            ></span>
+            <span
+              onClick={() => setSearch(!search)}
+              className="cursor-pointer text-xl text-gray-300 dark:text-white mr-5 bi bi-search"
+            ></span>
+            <Link to="/cart" className="text-gray-300 dark:text-white mr-5">
+              <span className="text-2xl bi bi-cart-fill relative">
+                <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">
+                  0
+                </span>
+              </span>
+            </Link>
+            <span onClick={()=>setDropdown(!dropDown)} className="bi bi-person-circle cursor-pointer text-2xl text-gray-300 dark:text-white"></span>
+            {dropDown && <DropdownLoggedOut /> }
           </div>
+        </div>
       </nav>
-     {search && <Search setSearch={setSearch} /> }  
+      {search && <Search setSearch={setSearch} />}
+    
     </header>
-  )
-}
+  );
+};
