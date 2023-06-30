1220643d264b7c64abd793244a57aad9af43d936
George <alximik15@gmail.com>
2023-06-30 15:53:58 +0400

    disabled items that are out of stock

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -76,12 +76,13 @@ export const ProductDetail = () => {
             <p className="my-3">
             {!inCart && <button
                 onClick={() => addToCart(product)}
-                className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800`}
+                className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${product.in_stock ? '': 'cursor-not-allowed'}`}
+                disabled={product.in_stock ? "" : "disabled"}
               >
                 Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
               </button> } 
             {inCart && <button onClick={() => removeFromCart(product)}
-                className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800`}
+                className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 `}
                 disabled={product.in_stock ? "" : "disabled"}
               >
                 Remove Item <i className="ml-1 bi bi-trash3"></i>
