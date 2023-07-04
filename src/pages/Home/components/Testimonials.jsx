c20b7c88f1bd8ee9457cd8a6592db2c1f86a13de
George <alximik15@gmail.com>
2023-07-04 18:54:36 +0400

    Changed Testimonials

--- src/pages/Home/components/Testimonials.jsx
+++ src/pages/Home/components/Testimonials.jsx
@@ -3,7 +3,7 @@ export const Testimonials = () => {
     <section className='my-20'>
         <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-5 underline underline-offset-8">Student About CodeBook</h1>    
         <div className="grid mb-8 rounded-lg border border-gray-200 shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
-            <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
+            <figure className="flex flex-col justify-center items-center p-8 text-center bg-dark rounded-t-lg border-b border-gray-200 md:rounded-t-none md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
                     <p className="my-4 font-light">If you care for your time, I hands down would go with this."</p>
@@ -16,9 +16,9 @@ export const Testimonials = () => {
                     </div>
                 </figcaption>    
             </figure>
-            <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-tr-lg border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+            <figure className="flex flex-col justify-center items-center p-8 text-center bg-dark rounded-tr-lg border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
-                    <h3 className="text-lg font-semibold  dark:text-gray-900">Solid foundation for any projects</h3>
+                    <h3 className="text-lg font-semibold  dark:white">Solid foundation for any projects</h3>
                     <p className="my-4 font-light">Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!"</p>
                 </blockquote>
                 <figcaption className="flex justify-center items-center space-x-3">
@@ -29,7 +29,7 @@ export const Testimonials = () => {
                     </div>
                 </figcaption>    
             </figure>
-            <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
+            <figure className="flex flex-col justify-center items-center p-8 text-center bg-dark rounded-bl-lg border-b border-gray-200 md:border-b-0 md:border-r dark:bg-gray-800 dark:border-gray-700">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Mindblowing workflow</h3>
                     <p className="my-4 font-light">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application."</p>
@@ -42,7 +42,7 @@ export const Testimonials = () => {
                     </div>
                 </figcaption>    
             </figure>
-            <figure className="flex flex-col justify-center items-center p-8 text-center bg-white rounded-b-lg border-gray-200 md:rounded-br-lg dark:bg-gray-800 dark:border-gray-700">
+            <figure className="flex flex-col justify-center items-center p-8 text-center bg-dark rounded-b-lg border-gray-200 md:rounded-br-lg dark:bg-gray-800 dark:border-gray-700">
                 <blockquote className="mx-auto mb-4 max-w-2xl text-gray-500 lg:mb-8 dark:text-gray-400">
                     <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Efficient Collaborating</h3>
                     <p className="my-4 font-light">You have many examples that can be used to create a fast prototype for your team."</p>
