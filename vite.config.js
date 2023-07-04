0414e50eac17e41cc2d4e554c1a38edb2cf82044
George <alximik15@gmail.com>
2023-07-04 16:11:31 +0400

    Changed Vite again

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import 'dotenv/config'; // Corrected import statement
+import path from 'path';// Corrected import statement
 
-dotenv.config();
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
